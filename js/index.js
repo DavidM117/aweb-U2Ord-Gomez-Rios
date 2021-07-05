d707052effbf3e07e055a7b3d8e9ab8d8401f074
Alexis David Gomez Rios <20203tn132@utez.edu.mx>
2021-07-05 17:41:48 -0500

    subida

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 
+
 function genera_nota(){
  
     console.log("Se llamó a generar-nota");
@@ -7,13 +8,20 @@ function genera_nota(){
     console.log(nt);
 
     limpiar();
+    var br = document.createElement("br");
 
     var newDiv = document.createElement("div");
-     var newContent = document.createTextNode(nt);
+    var newContent = document.createTextNode(nt);
     newDiv.appendChild(newContent); 
-
+    document.body.appendChild(br);
     var currentDiv = document.getElementById("div1");
     document.body.insertBefore(newDiv, currentDiv);
+
+    newDiv.style.color = "#ffffff"
+    newDiv.style.background = "gray"
+    newDiv.setAttribute("class","container");
+    document.body.appendChild(newDiv);
+
 }
 
 //------------------------------
@@ -28,7 +36,7 @@ function limpiar() {
 }
 //------------------------------
 
-   
+
 
         
     
